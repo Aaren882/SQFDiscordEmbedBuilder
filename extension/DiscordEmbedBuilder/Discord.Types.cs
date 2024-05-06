a2b9aa29cef09b7f6d4e30f9192b641badcf3062
Aaren <aaren.yang@gmail.com>
2024-05-06 14:21:22 +0800

    test

--- extension/DiscordEmbedBuilder/Discord.Types.cs
+++ extension/DiscordEmbedBuilder/Discord.Types.cs
@@ -8,6 +8,7 @@ namespace DiscordEmbedBuilder
             public string Title { get; set; }
             public string Description { get; set; }
             public string Color { get; set; }
+            public string timestamp { get; set; }
             public string AuthorName { get; set; }
             public string AuthorUrl { get; set; }
             public string AuthorIconUrl { get; set; }
@@ -21,11 +22,12 @@ namespace DiscordEmbedBuilder
                 Title = data.Count > 0 ? (string)data[0] : "";
                 Description = data.Count > 1 ? (string)data[1] : "";
                 Color = data.Count > 2 ? (string)data[2] : "14177041";
+                timestamp = (data[3].ToLower() == "true") ? DateTime.UtcNow.ToString("s") : "";
 
                 AuthorName = data.Count > 3 ? (string)data[3] : "";
                 AuthorUrl = data.Count > 4 ? (string)data[4] : "";
                 AuthorIconUrl = data.Count > 5 ? (string)data[5] : "";
-                
+
                 ImageUrl = data.Count > 6 ? (string)data[6] : "";
                 ThumbnailUrl = data.Count > 7 ? (string)data[7] : "";
                 FooterText = data.Count > 8 ? (string)data[8] : "";
