a596178949349f19529362ca36dba72f0df2abfc
Aaren <aaren.yang@gmail.com>
2024-05-06 20:06:57 +0800

    test

--- extension/DiscordEmbedBuilder/Discord.cs
+++ extension/DiscordEmbedBuilder/Discord.cs
@@ -33,7 +33,7 @@ namespace DiscordEmbedBuilder
                     // Build embeds array
                     //- Turn Data into List<List<string>> e.g [ ["TITLE","DESC"] , ["11","22] ]
                     List<List<string>> embedsData = ParseStringToList(args[6]);
-                    List<List<string>> FieldsData = ParseStringToList(args[7]);
+                    awiait List<List<string>> FieldsData = ParseStringToList(args[7]);
 
                     for (int i = 0; i < embedsData.Count; i++)
                     {
