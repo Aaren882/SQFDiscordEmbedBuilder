6ffadd376a821a5399032017200878d3445d1dcf
Aaren <aaren.yang@gmail.com>
2024-05-15 00:13:09 +0800

    Fix debug function

--- extension/DiscordMessageAPI/DLLEntry.cs
+++ extension/DiscordMessageAPI/DLLEntry.cs
@@ -50,64 +50,63 @@ namespace DiscordMessageAPI
             [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount)
         {
             outputSize--;
-            if (inputKey == "init_server" || inputKey == "init_player" || inputKey == "Refresh_Webhooks")
+            try
             {
-                // Use time as Key (for Server , Player)
-                if (ExtensionInit && inputKey != "Refresh_Webhooks")
+                if (inputKey == "init_server" || inputKey == "init_player" || inputKey == "Refresh_Webhooks")
                 {
-                    output.Append("Extension has already been initiated.");
-                    return -1;
-                }
-
-                // Get all Webhooks
-                if (inputKey == "init_server" || inputKey == "Refresh_Webhooks")
-                {
-                    string jsonString = File.ReadAllText($@"{Tools.AssemblyPath}\Webhooks.json");
-                    ALLWebhooks = JsonSerializer.Deserialize<Webhooks_Storage>(jsonString);
-                    int webhook_sel = Int32.Parse(args[0]);
-                    ExtensionInit = true;
-
-                    output.Append($"[\"{ALLWebhooks.Webhooks[webhook_sel]}\",\"{InitTime}\"]");
-                }
-                else //- Initation for Clients (Players)
-                    InitTime = args[0];
-
-                if (ALLWebhooks.Webhooks.Length == 0)
-                {
-                    output.Append("No Webhook Exist.");
-                    return -12;
-                }
-                return 1;
-            }
-            else
-            {
-                if (inputKey == InitTime)
-                {
-                    if (argCount == 8) // async without await because we don't expect a reply
+                    // Use time as Key (for Server , Player)
+                    if (ExtensionInit && inputKey != "Refresh_Webhooks")
                     {
-                        Discord.HandleRequest(args);
+                        output.Append("Extension has already been initiated.");
+                        return -1;
                     }
-                    else
+
+                    // Get all Webhooks
+                    if (inputKey == "init_server" || inputKey == "Refresh_Webhooks")
                     {
-                        output.Append("INCORRECT NUMBER OF ARGUMENTS");
-                        return -2;
+                        string jsonString = File.ReadAllText($@"{Tools.AssemblyPath}\Webhooks.json");
+                        ALLWebhooks = JsonSerializer.Deserialize<Webhooks_Storage>(jsonString);
+                        int webhook_sel = Int32.Parse(args[0]);
+                        ExtensionInit = true;
+
+                        output.Append($"[\"{ALLWebhooks.Webhooks[webhook_sel]}\",\"{InitTime}\"]");
                     }
+                    else //- Initation for Clients (Players)
+                        InitTime = args[0];
+
+                    if (ALLWebhooks.Webhooks.Length == 0)
+                    {
+                        output.Append("No Webhook Exist.");
+                        return -12;
+                    }
+                    return 1;
                 }
                 else
-                    output.Append("Find No Key.");
-            }
+                {
+                    if (inputKey == InitTime)
+                    {
+                        if (argCount == 8) // async without await because we don't expect a reply
+                        {
+                            Discord.HandleRequest(args);
+                        }
+                        else
+                        {
+                            output.Append("INCORRECT NUMBER OF ARGUMENTS");
+                            return -2;
+                        }
+                    }
+                    else
+                        output.Append("Find No Key.");
+                }
 
-            return 0;
-            /*try
-            {
-                
+                return 0;
             }
             catch (Exception e)
             {
                 Tools.Logger(e,$"{e}");
                 output.Append("Error!! Check Log.");
                 return -11;
-            }*/
+            }
         }
     }
 }
