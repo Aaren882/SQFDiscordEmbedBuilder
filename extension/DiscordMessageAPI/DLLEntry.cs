5d2750a7638dc57b4fd715169266e4dcd4a1edb3
Aaren <aaren.yang@gmail.com>
2026-02-14 14:18:15 +0800

    Refactor DLL entry to delegate-based command dispatch
    - Move command handling to new `EntryDelegates` class using a dictionary of delegates for modularity
    - Replace switch/case logic in `DLLEntry.cs` with delegate dispatch
    - Update `Webhooks_Storage` and `MsgPayload` types for clarity and correctness
    - Simplify `Discord.cs` handlers and add explicit TLS support
    - Add debug `Trace()` method and improve path handling in `Tools.cs`
    - Update publish profile for easier deployment

--- extension/DiscordMessageAPI/DLLEntry.cs
+++ extension/DiscordMessageAPI/DLLEntry.cs
@@ -1,234 +1,238 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
+using static DiscordMessageAPI.Delegates.EntryDelegates;
 
 namespace DiscordMessageAPI
 {
-    internal struct CallContext
-    {
-        public UInt64 steamId;
-        public string fileSource;
-        public string missionName;
-        public string serverName;
-        public Int16 remoteExecutedOwner;
-    };
+	internal struct CallContext
+	{
+		public UInt64 steamId;
+		public string fileSource;
+		public string missionName;
+		public string serverName;
+		public Int16 remoteExecutedOwner;
+	};
 
-    
+	public class DllEntry
+	{
+		//private static readonly string SessionKey = Tools.GenTimeEncode();
+		public static string InitTime = null;
+		public static bool ExtensionInit = false;
+		public static Webhooks_Storage? ALLWebhooks = null;
+		private static CallContext contextInfo;
+		internal static OutputBuilder CurrentOutputBuilder;
 
-    public class DllEntry
-    {
-        //private static readonly string SessionKey = Tools.GenTimeEncode();
-        public static string InitTime = null;
-        public static bool ExtensionInit = false;
-        public static Webhooks_Storage? ALLWebhooks = null;
-        private static CallContext contextInfo;
-        internal static OutputBuilder CurrentOutputBuilder;
+		private static void Output(IntPtr destination, int outputSize, string data)
+		{
+			var bytes = Encoding.UTF8.GetBytes(data);
+			Marshal.Copy(bytes, 0, destination, Math.Min(bytes.Length, outputSize));
+		}
 
-        private static void Output(IntPtr destination, int outputSize, string data)
-        {
-            var bytes = Encoding.UTF8.GetBytes(data);
-            Marshal.Copy(bytes, 0, destination, Math.Min(bytes.Length, outputSize));
-        }
+		private static void SetOutput(OutputBuilder Builder)
+		{
+			CurrentOutputBuilder = Builder;
+		}
 
-        private static void SetOutput(OutputBuilder Builder)
-        {
-            CurrentOutputBuilder = Builder;
-        }
+		internal class OutputBuilder
+		{
+			nint destination;
+			int outputSize;
 
-        internal class OutputBuilder
-        {
-            nint destination;
-            int outputSize;
+			public OutputBuilder(IntPtr destination, int outputSize)
+			{
+				this.destination = destination;
+				this.outputSize = outputSize;
+			}
 
-            public OutputBuilder(IntPtr destination, int outputSize)
-            {
-                this.destination = destination;
-                this.outputSize = outputSize;
-            }
+			/// <summary>
+			/// Construct output buffer for Arma
+			/// </summary>
+			/// <param name="data">String data that will be output</param>
+			public void Append(string data)
+			{
+				var bytes = Encoding.UTF8.GetBytes(data);
+				Marshal.Copy(bytes, 0, this.destination, Math.Min(bytes.Length, this.outputSize));
+			}
+		}
 
-            /// <summary>
-            /// Construct output buffer for Arma
-            /// </summary>
-            /// <param name="data">String data that will be output</param>
-            public void Append(string data)
-            {
-                var bytes = Encoding.UTF8.GetBytes(data);
-                Marshal.Copy(bytes, 0, this.destination, Math.Min(bytes.Length, this.outputSize));
-            }
-        }
+		/// <summary>
+		/// Gets called when Arma starts up and loads all extension.
+		/// It's perfect to load in static objects in a separate thread so that the extension doesn't needs any separate initalization
+		/// </summary>
+		/// <param name="output"></param>
+		/// <param name="outputSize"></param>
+		[UnmanagedCallersOnly(EntryPoint = "RvExtensionVersion")]
+		public static void RvExtensionVersion(nint output, int outputSize)
+		{
+			Output(output, outputSize, "Nigga");
+		}
 
-        /// <summary>
-        /// Gets called when Arma starts up and loads all extension.
-        /// It's perfect to load in static objects in a separate thread so that the extension doesn't needs any separate initalization
-        /// </summary>
-        /// <param name="output"></param>
-        /// <param name="outputSize"></param>
-        [UnmanagedCallersOnly(EntryPoint = "RvExtensionVersion")]
-        public static void RvExtensionVersion(nint output, int outputSize)
-        {
-            Output(output, outputSize, "Nigga");
-        }
+		[UnmanagedCallersOnly(EntryPoint = "RVExtensionContext")]
+		public static void RVExtensionContext(nint argsPtr, int argCount)
+		{
+			var args = new string?[argCount];
 
-        [UnmanagedCallersOnly(EntryPoint = "RVExtensionContext")]
-        public static void RVExtensionContext(nint argsPtr, int argCount)
-        {
-            var args = new string?[argCount];
+			for (int i = 0; i < argCount; i++)
+			{
+				var str = Marshal.PtrToStringUTF8(Marshal.ReadIntPtr(argsPtr + (i * Marshal.SizeOf<nint>())));
+				args[i] = str;
+			}
 
-            for (int i = 0; i < argCount; i++)
-            {
-                var str = Marshal.PtrToStringUTF8(Marshal.ReadIntPtr(argsPtr + (i * Marshal.SizeOf<nint>())));
-                args[i] = str;
-            }
+			contextInfo.steamId = Convert.ToUInt64(args[0]);
+			contextInfo.fileSource = args[1];
+			contextInfo.missionName = args[2];
+			contextInfo.serverName = args[3];
+			contextInfo.remoteExecutedOwner = Convert.ToInt16(args[4]);
+		}
 
-            contextInfo.steamId = Convert.ToUInt64(args[0]);
-            contextInfo.fileSource = args[1];
-            contextInfo.missionName = args[2];
-            contextInfo.serverName = args[3];
-            contextInfo.remoteExecutedOwner = Convert.ToInt16(args[4]);
-        }
+		/// <summary>
+		/// The entry point for the callExtensionArgs command.
+		/// </summary>
+		/// <param name="outputPrt"></param>
+		/// <param name="outputSize"></param>
+		/// <param name="function"></param>
+		/// <param name="argsPrt"></param>
+		/// <param name="argCount"></param>
+		/// <returns>
+		///     numbers
+		/// </returns>
+		[UnmanagedCallersOnly(EntryPoint = "RVExtensionArgs")]
+		public static int RvExtensionArgs(nint outputPrt, int outputSize, nint function, nint argsPrt, int argCount)
+		{
+			OutputBuilder output = new(outputPrt, outputSize);
+			SetOutput(output);
 
-        /// <summary>
-        /// The entry point for the callExtensionArgs command.
-        /// </summary>
-        /// <param name="outputPrt"></param>
-        /// <param name="outputSize"></param>
-        /// <param name="function"></param>
-        /// <param name="argsPrt"></param>
-        /// <param name="argCount"></param>
-        /// <returns>
-        ///     numbers
-        /// </returns>
-        [UnmanagedCallersOnly(EntryPoint = "RVExtensionArgs")]
-        public static int RvExtensionArgs(nint outputPrt, int outputSize, nint function, nint argsPrt, int argCount)
-        {
-            OutputBuilder output = new(outputPrt, outputSize);
-            SetOutput(output);
+			var inputKey = Marshal.PtrToStringUTF8(function);
+			var args = new string?[argCount];
 
-            var inputKey = Marshal.PtrToStringUTF8(function);
-            var args = new string?[argCount];
+			for (int i = 0; i < argCount; i++)
+			{
+				var str = Marshal.PtrToStringUTF8(Marshal.ReadIntPtr(argsPrt + (i * Marshal.SizeOf<nint>())));
+				args[i] = str = str
+					.Trim('"', ' ') //- Remove Arma quotations
+                    .Replace("\"\"", "\"");
 
-            for (int i = 0; i < argCount; i++)
-            {
-                var str = Marshal.PtrToStringUTF8(Marshal.ReadIntPtr(argsPrt + (i * Marshal.SizeOf<nint>())));
-                args[i] = str;
-            }
-
-            try
-            {
-                // Use time as Key (for Server , Player)
-                if (ExtensionInit && inputKey == "init_player")
-                {
-                    output.Append("Extension has already been initiated.");
-                    return -1;
-                }
-
-                // Remove arma quotations
+				Tools.Trace($"DLL Entry => \"{i}\"", $"\"str = {str}\"");
                 //args = args.Select(arg => arg.Trim('"', ' ').Replace("\"\"", "\"")).ToArray();
-
-                //Entry
-                switch (inputKey == "init_player" || inputKey == "Refresh_Webhooks")
-                {
-                    //- Init Functions 
-                    case true:
-                    {
-                        // Get all Webhooks
-                        if (inputKey == "Refresh_Webhooks")
-                        {
-                            string jsonString = Tools.ParseJson("Webhooks.json");
-
-                            ALLWebhooks = JsonSerializer.Deserialize<Webhooks_Storage>(
-                                jsonString,
-                                Webhooks_Storage_JsonContext.Default.Webhooks_Storage);
-
-
-                            int webhooksCount = ALLWebhooks!.Webhooks.Length;
-                            int webhook_sel = Math.Min(Int32.Parse(args[0]), webhooksCount - 1);
-                            ExtensionInit = true;
-
-                            //- Exit if there's no Webhook
-                            if (webhooksCount == 0)
-                            {
-                                output.Append("No Webhook Exist.");
-                                return 0;
-                            }
-
-                            if (webhook_sel < 0) // output can be like ["ww", "ww"]
-                                output.Append($"[[\"{string.Join("\",\"", ALLWebhooks.Webhooks)}\"],\"{InitTime}\"]");
-                            else
-                                output.Append($"[\"{ALLWebhooks.Webhooks[webhook_sel]}\",\"{InitTime}\"]");
-
-                            return webhooksCount;
-                        }
-                        else //- Initation for Clients (Players)
-                            InitTime = args[0]; //- From Server
-                        break;
-                    }
-                    default:
-                    {
-                        if (inputKey == "ParseJson")
-                        {
-                            int[] utf = Tools.StringToCode32(Tools.ParseJson(args[0]));
-                            output.Append($"[{string.Join(",", utf)}]");
-                            break;
-                        }
-                        if (InitTime == null)
-                        {
-                            output.Append("Find No Key.");
-                            break;
-                        }
-
-                        //- args[0] :
-                        //- Http(s) Handlers ["url", HandlerType<int>, Optional :[Necessary Payload] ]
-                        switch (inputKey)
-                        {
-                            //- Load Json as Message format
-                            case "HandlerJson":
-                                {
-                                    Discord.HandlerJson(args);
-                                    break;
-                                }
-                            case "HandlerJsonFormat":
-                                {
-                                    Discord.HandlerJsonFormat(args);
-                                    break;
-                                }
-                            case "SendMessage":
-                                {
-                                    if (argCount == 8) // async without await because we don't expect a reply
-                                    {
-                                        string[] codePointStrings = Regex.Replace(args[5], @"[\[\]]", "").Split(',');
-                                        if (codePointStrings.Length > 1)
-                                            args[5] = string.Concat(codePointStrings.Select(cp => char.ConvertFromUtf32(int.Parse(cp))));
-                                        Discord.HandleRequest(args);
-                                    }
-                                    else
-                                    {
-                                        output.Append("INCORRECT NUMBER OF ARGUMENTS");
-                                        return -2;
-                                    }
-                                    break;
-                                }
-                            default: //- Other conditions
-                                break;
-                        }
-
-                        break; //- Exit
-                    }
-                }
-
-                return 0;
             }
-            catch (Exception e)
-            {
-                //output.Append($"webhook : {ALLWebhooks!}");
-                output.Append($"Error!! \"{e.Message}\"");
 
-                Tools.Logger(e);
-                return -11;
-            }
-        }
-    }
+			try
+			{
+				Tools.Trace("DLL Entry", inputKey);
+				InitActions action = ActionsDict.GetValueOrDefault(inputKey, EntryActions.NullDefault);
+				int actionReturn = action(output, args, argCount);
+
+				if (InitTime == null)
+					throw new Exception("Find No Key.");
+
+				// Use time as Key (for Server , Player)
+				/*if (ExtensionInit && inputKey == "init_player")
+				{
+					output.Append("Extension has already been initiated.");
+					return -1;
+				}*/
+
+				//Entry
+				/*switch (inputKey == "init_player" || inputKey == "Refresh_Webhooks")
+				{
+					//- Init Functions 
+					case true:
+					{
+							// Get all Webhooks
+							if (inputKey == "Refresh_Webhooks")
+							{
+								string jsonString = Tools.ParseJson("Webhooks.json");
+
+								ALLWebhooks = JsonSerializer.Deserialize<Webhooks_Storage>(
+									jsonString,
+									Webhooks_Storage_JsonContext.Default.Webhooks_Storage);
+
+
+								int webhooksCount = ALLWebhooks!.Webhooks.Length;
+								int webhook_sel = Math.Min(Int32.Parse(args[0]), webhooksCount - 1);
+								ExtensionInit = true;
+
+								//- Exit if there's no Webhook
+								if (webhooksCount == 0)
+								{
+									output.Append("No Webhook Exist.");
+									return 0;
+								}
+
+								if (webhook_sel < 0) // output can be like ["ww", "ww"]
+									output.Append($"[[\"{string.Join("\",\"", ALLWebhooks.Webhooks)}\"],\"{InitTime}\"]");
+								else
+									output.Append($"[\"{ALLWebhooks.Webhooks[webhook_sel]}\",\"{InitTime}\"]");
+
+								return webhooksCount;
+							}
+							else //- Initation for Clients (Players)
+								InitTime = args[0]; //- From Server
+
+						break;
+					}
+					default:
+					{
+						if (inputKey == "ParseJson")
+						{
+							int[] utf = Tools.StringToCode32(Tools.ParseJson(args[0]));
+							output.Append($"[{string.Join(",", utf)}]");
+							break;
+						}
+						if (InitTime == null)
+						{
+							output.Append("Find No Key.");
+							break;
+						}
+
+						//- args[0] :
+						//- Http(s) Handlers ["url", HandlerType<int>, Optional :[Necessary Payload] ]
+						switch (inputKey)
+						{
+							//- Load Json as Message format
+							case "HandlerJson":
+								{
+									Discord.HandlerJson(args);
+									break;
+								}
+							case "HandlerJsonFormat":
+								{
+									Discord.HandlerJsonFormat(args);
+									break;
+								}
+							case "SendMessage":
+								{
+									if (argCount == 8) // async without await because we don't expect a reply
+									{
+										string[] codePointStrings = Regex.Replace(args[5], @"[\[\]]", "").Split(',');
+										if (codePointStrings.Length > 1)
+											args[5] = string.Concat(codePointStrings.Select(cp => char.ConvertFromUtf32(int.Parse(cp))));
+										Discord.HandleRequest(args);
+									}
+									else
+									{
+										output.Append("INCORRECT NUMBER OF ARGUMENTS");
+										return -2;
+									}
+									break;
+								}
+							default: //- Other conditions
+								break;
+						}
+
+						break; //- Exit
+					}
+				}*/
+
+				return 0;
+			}
+			catch (Exception e)
+			{
+				output.Append($"Error!! \"{e.Message}\"");
+				Tools.Logger(e);
+
+				return -11;
+			}
+		}
+	}
 }
---
+++ extension/DiscordMessageAPI/Delegates/EntryDelegates.cs
@@ -0,0 +1,172 @@
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
+using static DiscordMessageAPI.DllEntry;
+
+namespace DiscordMessageAPI.Delegates
+{
+    internal class EntryDelegates
+    {
+        internal static readonly Dictionary<string, InitActions> ActionsDict = _Init();
+        internal delegate int InitActions(OutputBuilder output, string[] args, int argCount);
+
+
+        private static Dictionary<string, InitActions> _Init()
+        {
+            var _dict = new Dictionary<string, InitActions>();
+    
+            string[] keys = [
+                "init_player", "Refresh_Webhooks",
+                "ParseJson", "HandlerJson", "HandlerJsonFormat", "SendMessage"
+            ];
+
+            InitActions[] actions = [
+                EntryActions.Init_player, EntryActions.Refresh_Webhooks,
+                Actions.ParseJson, Actions.HandlerJson, Actions.HandlerJsonFormat, Actions.SendMessage
+            ];
+
+            //- Register functions
+            for (int i = 0; i < keys.Length; i++)
+            {
+                var key = keys[i];
+                var action = actions[i];
+                _dict.Add(key, action);
+            }
+
+            return _dict;
+        }
+
+        internal static class EntryActions
+        {
+            /// <summary>
+            /// Initation for Clients (Players)
+            /// </summary>
+            /// <param name="output"></param>
+            /// <param name="args"></param>
+            /// <returns></returns>
+            internal static int Init_player(OutputBuilder output, string[] args, int argCount)
+            {
+                if (ExtensionInit)
+                {
+                    throw new Exception("Extension has already been initiated.");
+                }
+                InitTime = args[0]; //- From Server
+                return 0;
+            }
+            /// <summary>
+            /// Fetch discord webhooks from `Webhooks.json`
+            /// </summary>
+            /// <param name="output"></param>
+            /// <param name="args"></param>
+            /// <returns></returns>
+            internal static int Refresh_Webhooks(OutputBuilder output, string[] args, int argCount)
+            {
+                string jsonString = Tools.ParseJson("Webhooks.json");
+                Tools.Trace("Refresh_Webhooks", jsonString);
+
+                ALLWebhooks = JsonSerializer.Deserialize<Webhooks_Storage>(
+                    jsonString,
+                    Webhooks_Storage_JsonContext.Default.Webhooks_Storage);
+
+
+                int webhooksCount = ALLWebhooks!.Webhooks.Length;
+                int webhook_sel = Math.Min(Int32.Parse(args[0]), webhooksCount - 1);
+                ExtensionInit = true;
+
+                //- Exit if there's no Webhook
+                if (webhooksCount == 0)
+                {
+                    throw new Exception("No Webhook Exist.");
+                }
+
+                if (webhook_sel < 0) // output can be like ["ww", "ww"]
+                    output.Append($"[[\"{string.Join("\",\"", ALLWebhooks.Webhooks)}\"],\"{InitTime}\"]");
+                else
+                    output.Append($"[\"{ALLWebhooks.Webhooks[webhook_sel]}\",\"{InitTime}\"]");
+
+                return webhooksCount;
+            }
+
+            /// <summary>
+            /// Default method for **ActionsDict**
+            /// </summary>
+            /// <param name="output"></param>
+            /// <param name="args"></param>
+            /// <returns></returns>
+            /// <exception cref="Exception"></exception>
+            internal static int NullDefault(OutputBuilder output, string[] args, int argCount)
+            {
+                throw new Exception("No method found.");
+            }
+        }
+
+        internal static class Actions
+        {
+            /// <summary>
+            /// Parses the first JSON string in the specified arguments, converts it to a UTF-32 code point array, and
+            /// appends the result to the output.
+            /// </summary>
+            /// <param name="output">The output builder to which the UTF-32 code point array representation will be appended.</param>
+            /// <param name="args">An array of strings containing the arguments. The first element is expected to be a JSON string to
+            /// parse.</param>
+            /// <param name="argCount">The number of arguments provided in the args array.</param>
+            /// <returns>Always returns 1 to indicate successful processing of the first argument.</returns>
+            internal static int ParseJson(OutputBuilder output, string[] args, int argCount)
+            {
+                int[] utf = Tools.StringToCode32(Tools.ParseJson(args[0]));
+                output.Append($"[{string.Join(",", utf)}]");
+                return 1;
+            }
+            /// <summary>
+            /// Handles a JSON-related command using the specified arguments.
+            /// </summary>
+            /// <param name="output">The output builder used to construct the command's response. This parameter is not modified by this
+            /// method.</param>
+            /// <param name="args">An array of command-line arguments to process.</param>
+            /// <param name="argCount">The number of arguments provided in the <paramref name="args"/> array.</param>
+            /// <returns>Always returns 1 to indicate successful handling of the command.</returns>
+            internal static int HandlerJson(OutputBuilder output, string[] args, int argCount)
+            {
+                Discord.HandlerJson(args);
+                return 1;
+            }
+            /// <summary>
+            /// Formats the first argument as a JSON string, converts it to a UTF-32 code point array, and appends the
+            /// result to the specified output.
+            /// </summary>
+            /// <param name="output">The output builder to which the formatted UTF-32 code point array will be appended.</param>
+            /// <param name="args">An array of arguments, where the first element is expected to be a JSON-formatted string to process.</param>
+            /// <param name="argCount">The number of arguments provided in the <paramref name="args"/> array.</param>
+            /// <returns>Always returns 1 to indicate successful processing.</returns>
+            internal static int HandlerJsonFormat(OutputBuilder output, string[] args, int argCount)
+            {
+                Discord.HandlerJsonFormat(args);
+                return 1;
+            }
+            /// <summary>
+            /// Processes and sends a message using the specified arguments and output builder.
+            /// </summary>
+            /// <remarks>If the sixth argument contains multiple Unicode code points enclosed in
+            /// brackets, they are converted to their corresponding characters before sending the message. This method
+            /// does not expect a reply and is intended for asynchronous use.</remarks>
+            /// <param name="output">The output builder used to append status or error messages during processing.</param>
+            /// <param name="args">An array of strings containing the arguments required to construct and send the message. The array must
+            /// contain at least six elements, with the sixth element representing code points or message content.</param>
+            /// <param name="argCount">The number of arguments provided in the <paramref name="args"/> array. Must be exactly 8.</param>
+            /// <returns>An integer value of 1 if the message is processed and sent successfully.</returns>
+            /// <exception cref="Exception">Thrown if <paramref name="argCount"/> is not equal to 8.</exception>
+            internal static int SendMessage(OutputBuilder output, string[] args, int argCount)
+            {
+                if (argCount != 8) // async without await because we don't expect a reply
+                    throw new Exception("INCORRECT NUMBER OF ARGUMENTS");
+
+                string[] codePointStrings = Regex.Replace(args[5], @"[\[\]]", "").Split(',');
+
+                if (codePointStrings.Length > 1)
+                    args[5] = string.Concat(codePointStrings.Select(cp => char.ConvertFromUtf32(int.Parse(cp))));
+
+                Discord.HandleRequest(args);
+                return 1;
+            }
+        }
+    }
+}
