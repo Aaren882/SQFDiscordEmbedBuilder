1d734e576131a78d6c867223a23db6d551942ef3
Aaren <aaren.yang@gmail.com>
2026-02-27 17:29:09 +0800

    Working Discord Bot

---
+++ extension/Arma3DiscordBot/Program.cs
@@ -0,0 +1,51 @@
+
+using Arma3DiscordBot.Controllers;
+using Arma3DiscordBot.Services;
+using Discord;
+using Discord.WebSocket;
+using DotNetEnv.Extensions;
+
+namespace Arma3DiscordBot
+{
+	public class Program
+	{
+		internal static readonly Dictionary<string, string> env = DotNetEnv.Env.Load().ToDotEnvDictionary();
+		internal static DiscordSocketClient? DiscordBotClient;
+
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			DiscordBotClient = new DiscordSocketClient();
+
+			// Add services to the container.
+
+			builder.Services.AddControllers();
+			// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+			//builder.Services.AddOpenApi();
+			builder.Services.AddSwaggerGen();
+
+			//- Regiester Bot Service
+			builder.Services.AddScoped<DiscordBotService>();
+			builder.Services.AddHostedService<DiscordBotService>();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				//app.MapOpenApi();
+				app.MapSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
+}
