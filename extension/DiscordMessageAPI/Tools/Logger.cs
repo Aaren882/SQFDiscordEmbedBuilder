094205c1ce2a99bff6863e9f24c3b289435d4b42
Aaren <aaren.yang@gmail.com>
2026-03-19 00:15:47 +0800

    Fix `Logger.CleanLogs` logic to correctly identify and delete the oldest log files.

--- extension/DiscordMessageAPI/Tools/Logger.cs
+++ extension/DiscordMessageAPI/Tools/Logger.cs
@@ -1,73 +1,72 @@
-namespace DiscordMessageAPI.Tools
+namespace DiscordMessageAPI.Tools;
+
+internal class Logger
 {
-	internal class Logger
+	private static readonly string ExtFilePath = Util.AssemblyPath!;
+	private static readonly string LogFilePath = Path.Combine(ExtFilePath, "logs");
+	private static readonly string LogFileName = Path.Combine(
+		LogFilePath,
+		$"{DateTime.Now.ToString("yyyy-MM-dd.HH-mm-ss")}.log");
+
+	/// <summary>
+	/// Writes a trace message to the logger if debugging is enabled.
+	/// </summary>
+	/// <remarks>No output is generated if debugging is not enabled. This method is intended for
+	/// internal diagnostic purposes.</remarks>
+	/// <param name="Name">The name or category associated with the trace message. Used to identify the source or context of the trace
+	/// output.</param>
+	/// <param name="content">The content of the trace message to be logged.</param>
+	internal static void Trace(string Name, string content)
 	{
-		private static readonly string ExtFilePath = Util.AssemblyPath!;
-		private static readonly string LogFilePath = Path.Combine(ExtFilePath, "logs");
-		private static readonly string LogFileName = Path.Combine(
-			LogFilePath,
-			$"{DateTime.Now.ToString("yyyy-MM-dd.HH-mm-ss")}.log");
-
-		/// <summary>
-		/// Writes a trace message to the logger if debugging is enabled.
-		/// </summary>
-		/// <remarks>No output is generated if debugging is not enabled. This method is intended for
-		/// internal diagnostic purposes.</remarks>
-		/// <param name="Name">The name or category associated with the trace message. Used to identify the source or context of the trace
-		/// output.</param>
-		/// <param name="content">The content of the trace message to be logged.</param>
-		internal static void Trace(string Name, string content)
-		{
 #if DEBUG
-			Log(null, $"TRACER - {Name} : {content}");
+		Log(null, $"TRACER - {Name} : {content}");
 #endif
-		}
-		internal static void Log(Exception? e, string s = "", bool loop = false)
+	}
+	internal static void Log(Exception? e, string s = "", bool loop = false)
+	{
+		try
 		{
-			try
-			{
-				if (!Directory.Exists(ExtFilePath))
-					Directory.CreateDirectory(ExtFilePath);
-				if (!Directory.Exists(LogFilePath))
-					Directory.CreateDirectory(LogFilePath);
+			if (!Directory.Exists(ExtFilePath))
+				Directory.CreateDirectory(ExtFilePath);
+			if (!Directory.Exists(LogFilePath))
+				Directory.CreateDirectory(LogFilePath);
 
-				using var file = new StreamWriter(LogFileName, true);
-				if (string.IsNullOrEmpty(s))
-					s = e!.Message;
-				if (s.Length > 0)
-					file.WriteLine($"{DateTime.Now:T} - {s}");
-			}
-			catch (Exception i)
-			{
-				if (!loop)
-					Log(i, null, true);
-			}
+			using var file = new StreamWriter(LogFileName, true);
+			if (string.IsNullOrEmpty(s))
+				s = e!.Message;
+			if (s.Length > 0)
+				file.WriteLine($"{DateTime.Now:T} - {s}");
 		}
-
-		internal static void CleanLogs()
+		catch (Exception i)
 		{
-			var limit = 10;
-			var files = Directory.GetFiles(LogFilePath);
-
-			//- Check how many logs
-			Trace("CleanLogs", "Check how many logs...");
-			if (files.Length < limit) return;
-
-			Dictionary<string, DateTime> dict = new();
-			foreach (var file in files)
-			{
-				var time = Directory.GetCreationTime(file);
-				dict.Add(file, time);
-			}
-
-			var list = dict.OrderByDescending(x => x.Value).ToList();
-			for (var i = 0; i < list.Count - limit; i++)
-			{
-				var logFile= list[i].Key;
-				File.Delete(logFile);
-			}
-			
-			Trace("CleanLogs", "Old logs is cleaned out.");
+			if (!loop)
+				Log(i, null, true);
 		}
+	}
+
+	internal static void CleanLogs()
+	{
+		var limit = 10;
+		var files = Directory.GetFiles(LogFilePath);
+
+		//- Check how many logs
+		Trace("CleanLogs", "Check how many logs...");
+		if (files.Length < limit) return;
+
+		Dictionary<string, DateTime> dict = new();
+		foreach (var file in files)
+		{
+			var time = Directory.GetCreationTime(file);
+			dict.Add(file, time);
+		}
+
+		var list = dict.OrderBy(x => x.Value).ToList();
+		for (var i = 0; i < list.Count - limit; i++)
+		{
+			var logFile= list[i].Key;
+			File.Delete(logFile);
+		}
+		
+		Trace("CleanLogs", "Old logs is cleaned out.");
 	}
 }
