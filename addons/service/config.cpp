2b4cda0910e1ae58ec301b1df2158fd60f173330
Aaren <aaren.yang@gmail.com>
2026-03-20 19:56:00 +0800

    Add service component for extension callbacks

---
+++ addons/service/config.cpp
@@ -0,0 +1,23 @@
+#include "script_component.hpp"
+
+class CfgPatches
+{
+	class ADDON
+	{
+		authors[] = {"Aaren"};
+		url = ECSTRING(main,url);
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = {QUOTE(MAIN_ADDON)};
+		units[] = {};
+		weapons[] = {};
+		VERSION_CONFIG;
+	};
+};
+
+class Extended_PostInit_EventHandlers 
+{
+	class ADDON
+	{
+		serverInit = QUOTE(call COMPILE_FILE(XEH_postInit));
+	};
+};
