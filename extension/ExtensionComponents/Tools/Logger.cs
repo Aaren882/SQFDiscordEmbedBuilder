0e8fae2948a8d06499706cb8b647de03d5ca938b
Aaren <aaren.yang@gmail.com>
2026-07-07 05:25:23 +0800

    Introduce asynchronous logging using Dataflow
    - Implement Concurrent Logging: Refactor `LoggerBase` to utilize `System.Threading.Tasks.Dataflow.ActionBlock`, enabling non-blocking, asynchronous log processing.
    - Improve Thread Safety: Encapsulate logging operations within a private `LoggerObject` record, which is posted to the dedicated logging process block (`_loggerProcess`) for sequential and safe file writing.
    - Refine Class Structure: Change `LoggerBase` to a `sealed` class to prevent inheritance and ensure strict control over the logging implementation.
    - Decouple I/O Operations: Modify `Log` and `Trace` methods to queue log requests to the `ActionBlock` instead of performing immediate file I/O, significantly improving application responsiveness under load.

--- extension/ExtensionComponents/Tools/Logger.cs
+++ extension/ExtensionComponents/Tools/Logger.cs
@@ -1,14 +1,36 @@
+using System.Threading.Tasks.Dataflow;
 using ExtensionComponents.Entity;
 
 namespace ExtensionComponents.Tools;
 
-public class LoggerBase: ILogger
+public sealed class LoggerBase: ILogger
 {
 	private static readonly string ExtFilePath = Util.AssemblyPath!;
 	private static readonly string LogFilePath = Path.Combine(ExtFilePath, "logs");
 	private static readonly string LogFileName = Path.Combine(
 		LogFilePath,
 		$"{DateTime.Now:yyyy-MM-dd.HH-mm-ss}.log");
+
+	private record LoggerObject(Exception? e, string s)
+	{
+		public void WriteLog()
+		{
+			var s = this.s;
+			if (!Directory.Exists(ExtFilePath))
+				Directory.CreateDirectory(ExtFilePath);
+			if (!Directory.Exists(LogFilePath))
+				Directory.CreateDirectory(LogFilePath);
+		
+			using var file = new StreamWriter(LogFileName, true);
+			if (string.IsNullOrEmpty(s))
+				s = e!.Message;
+		
+			if (s.Length > 0)
+				file.WriteLine($"{DateTime.Now:T} - {s}");
+		}
+	};
+	private static readonly ActionBlock<LoggerObject> _loggerProcess = new (o => o.WriteLog(),
+		new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 1 });
 
 	public static void Trace(string Name, string content)
 	{
@@ -19,22 +41,12 @@ public class LoggerBase: ILogger
 
 	public static void Log(Exception? e, string s = "")
 	{
-		if (!Directory.Exists(ExtFilePath))
-			Directory.CreateDirectory(ExtFilePath);
-		if (!Directory.Exists(LogFilePath))
-			Directory.CreateDirectory(LogFilePath);
-
-		using var file = new StreamWriter(LogFileName, true);
-		if (string.IsNullOrEmpty(s))
-			s = e!.Message;
-		
-		if (s.Length > 0)
-			file.WriteLine($"{DateTime.Now:T} - {s}");
+		_loggerProcess.Post(new LoggerObject(e, s));
 	}
 
 	public static void CleanLogs()
 	{
-		var limit = 10;
+		const int limit = 10;
 		var files = Directory.GetFiles(LogFilePath);
 
 		//- Check how many logs
